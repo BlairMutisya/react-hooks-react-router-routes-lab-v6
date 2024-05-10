1b662237f856124ee3ba7f9567a584b97ed936e1
BlairMutisya <tonnyblair09@gmail.com>
2024-05-10 22:44:03 +0300

    Changes

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,6 +4,7 @@ function MovieCard({ title, id }) {
   return (
     <article>
       <h2>{title}</h2>
+      {/* What should go here? */}
       <Link to={`/movie/${id}`}>View Info</Link>
     </article>
   );
